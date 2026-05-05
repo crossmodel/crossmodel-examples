538cf5a849c153a89dda177120264aa5d5384812
Your Name <your@email.com>
2026-05-05 20:42:48 +0000

    Removed the customerwithordercount file. We will add it after group by is implemented in a nicer way.

--- dbt/BrightGreen/models/mapping/mapping_CustomerWithOrderCount.sql
+++
@@ -1,8 +0,0 @@
-with mapping_CustomerWithOrderCount as (
-    select
-        CustomerID AS Customer_ID,
-        COUNT(SalesOrder.SalesOrderID) AS OrderCount
-    from {{ ref('Customer') }} as Customer
-    left join {{ ref('SalesOrder') }} as SalesOrder
-        ON Customer.CustomerID = SalesOrder.CustomerID
-) select * from mapping_CustomerWithOrderCount
